52b602da1039f423d7959855b37b4d322030d6d7
kalous12 <2797795537@qq.com>
2022-10-10 16:17:36 +0800

    fix

--- base_linux/screen/image/bmp/sources/main.c
+++ base_linux/screen/image/bmp/sources/main.c
@@ -6,40 +6,62 @@ uint32_t color_table[6] = {RED,GREEN,BLUE,BLACK,WHITE,BLACK_BLUE};
 struct bmpfile {
 	FILE* fp;
 	int fd;
-	int file_size;		//文件中图像的大小-4Bytes
-	int bmp_offset;		//图像偏移量-4Bytes
-	int biSize;			//位图信息头大小-4Bytes
-	int biWidth;		//图像宽度-4Bytes
-	int biHeight;		//图像高度-4Bytes
-	short biPlanes;		//颜色平面书-2Bytes
-	short biBitCount;	//每Bit占用位数-2Bytes
-	int biCompression;	//数据压缩类型-4Bytes
-	int biSizeImage;	//图像数据大小-4Bytes
-	int biXPelsPerMeter;//像素/米-4Bytes
-	int biYPelsPerMeter;//像素/米-4Bytes
-	int biClrUsed;		//调色板索引数-4Bytes
-	int biClrImportant;	//索引数目-4Bytes
+	int file_size;				//文件中图像的大小-4Bytes
+	int bmp_offset;				//图像偏移量-4Bytes
+	int biSize;					//位图信息头大小-4Bytes
+	int biWidth;				//图像宽度-4Bytes
+	int biHeight;				//图像高度-4Bytes
+	short biPlanes;				//颜色平面书-2Bytes
+	short biBitCount;			//每像素占用位数-2Bytes
+	int biCompression;			//数据压缩类型-4Bytes
+	int biSizeImage;			//图像数据大小-4Bytes
+	int biXPelsPerMeter;		//像素/米-4Bytes
+	int biYPelsPerMeter;		//像素/米-4Bytes
+	int biClrUsed;				//调色板索引数-4Bytes
+	int biClrImportant;			//索引数目-4Bytes
 	unsigned char *mem_buf;		//内存映射，整个文件
 	unsigned char *bmp_buf;		//将bmp文件的格式改成图像rgb格式
+	unsigned char bpp;			//每个像素占用的字节数
 };
+
+
+void show_bmp_info(struct bmpfile *bf)
+{
+	//打印文件的内容
+	printf("file_size = %d\n",bf->file_size);
+	printf("bmp_offset = %d\n",bf->bmp_offset);
+	printf("biSize = %d\n",bf->biSize);
+	printf("biWidth = %d\n",bf->biWidth);
+	printf("biHeight = %d\n",bf->biHeight);
+	printf("biPlanes = %d\n",bf->biPlanes);
+	printf("biBitCount = %d\n",bf->biBitCount);
+	printf("biCompression = %d\n",bf->biCompression);
+	printf("biSizeImage = %d\n",bf->biSizeImage);
+	printf("biXPelsPerMeter = %d\n",bf->biXPelsPerMeter);
+	printf("biYPelsPerMeter = %d\n",bf->biYPelsPerMeter);
+	printf("biClrUsed = %d\n",bf->biClrUsed);
+	printf("biClrImportant = %d\n",bf->biClrImportant);
+}
+
 
 int judge_bmp(char *filename,struct bmpfile *pfd)
 {
 	int offset = 0;
 	int count;
 	char file_head[14];
-
+	//打开文件
 	pfd->fp = fopen(filename,"rb");
 	if(pfd->fp == NULL){
 		printf("open file fail\n");
 		return -1;
 	}
+	//获取前14个字节的内容
 	count = fread(file_head,1,14,pfd->fp);
 	if(count != 14){
 		printf("read file fail\n");
 		return -1;
 	}
-
+	//判断是否为BMP文件
 	if(file_head[0] == 0x42 &&  file_head[1] == 0x4d){
 		//解析参数
 		offset = 2;
@@ -74,6 +96,12 @@ int get_bmp_file(char *filename,struct bmpfile *bf)
 	bf->mem_buf = (unsigned char *)mmap(NULL , bf->file_size, 
 						PROT_READ | PROT_WRITE, MAP_SHARED, 
 						bf->fd, 0);
+	if(bf->mem_buf == NULL){
+		printf("mmap wrong !\n");
+		close(bf->fd);
+		return -1;
+	}
+
 	memcpy(&bf->biSize,bf->mem_buf+offset,4);
 	offset += 4;
 	memcpy(&bf->biWidth,bf->mem_buf+offset,4);
@@ -96,13 +124,23 @@ int get_bmp_file(char *filename,struct bmpfile *bf)
 	offset += 4;
 	memcpy(&bf->biClrImportant,bf->mem_buf+offset,4);
 
+	if(bf->biSizeImage == 0)
+		bf->biSizeImage = bf->file_size - bf->bmp_offset;
+	
+	if(bf->biBitCount == 24 || bf->biBitCount == 32 )
+		bf->bpp = bf->biBitCount/8;
+
+	show_bmp_info(bf);
 	//分配空间，操作指针需要分配空间
 	bf->bmp_buf = malloc(bf->biSizeImage);
 
 	//将bmp文件的格式改为RGB适用的格式
 	for(i = 0;i<bf->biHeight;i++)
-		memcpy( bf->bmp_buf + i*bf->biWidth*3 , bf->mem_buf +(bf->biHeight-1-i)*bf->biWidth*3+bf->bmp_offset,bf->biWidth * 3);
+		memcpy( bf->bmp_buf + i*bf->biWidth*bf->bpp , 
+				bf->mem_buf +(bf->biHeight-1-i)*bf->biWidth*bf->bpp+ bf->bmp_offset,
+				bf->biWidth * bf->bpp);
 
+	close(bf->fd);
 	return 0;
 }
 
@@ -113,44 +151,34 @@ int free_bmp_file(struct bmpfile *bf)
 	free(bf->bmp_buf);
 }
 
-int show_bmp(struct bmpfile *bf)
+//显示图片，根据X,Y坐标
+int show_bmp(struct bmpfile *bf,int x ,int y)
 {
-	int i;
+	int i,j;
 	uint32_t word;
-	if(bf->biBitCount == 24){
-		//显示图片
-		for(i=0;i<720*1280;i++){
-			word = 0;
-			word = ((word | bf->bmp_buf[i*3+2])<<16) | 
-					((word | bf->bmp_buf[i*3+1])<<8) | 
-					((word | bf->bmp_buf[i*3]));	
-			buf.vaddr[i] = word ;
+	if(x<0 || y<0){
+		printf("wrong set !\n");
+		return -1;
+	}
+		
+	if(bf->biBitCount == 24 || bf->biBitCount == 32){
+		for(j=0; j<bf->biHeight; j++){
+			for(i = 0 ; i<bf->biWidth;i++){
+				if((j+y) < buf.height && (i+x)<buf.width){
+					word = 0;
+					word = ((word | bf->bmp_buf[(j*bf->biWidth+i)*bf->bpp+2])<<16) | 
+						   ((word | bf->bmp_buf[(j*bf->biWidth+i)*bf->bpp+1])<<8) | 
+						   ((word | bf->bmp_buf[(j*bf->biWidth+i)*bf->bpp]));
+					buf.vaddr[(j+y)*buf.width+(x+i)] = word;
+				}
+				else
+					continue;
+			}
 		}
 	}
-	else{
-		printf("unsupport,you can use 24bpp bmp file to test\n");
-		return -1;	
-
-	}
+	else
+		printf("unsupport bpps!Please use 24bpps or 32bpps bmp file\n");
 	return 0;	
-}
-
-void show_bmp_info(struct bmpfile *bf)
-{
-	//打印文件的内容
-	printf("file_size = %d\n",bf->file_size);
-	printf("bmp_offset = %d\n",bf->bmp_offset);
-	printf("biSize = %d\n",bf->biSize);
-	printf("biWidth = %d\n",bf->biWidth);
-	printf("biHeight = %d\n",bf->biHeight);
-	printf("biPlanes = %d\n",bf->biPlanes);
-	printf("biBitCount = %d\n",bf->biBitCount);
-	printf("biCompression = %d\n",bf->biCompression);
-	printf("biSizeImage = %d\n",bf->biSizeImage);
-	printf("biXPelsPerMeter = %d\n",bf->biXPelsPerMeter);
-	printf("biYPelsPerMeter = %d\n",bf->biYPelsPerMeter);
-	printf("biClrUsed = %d\n",bf->biClrUsed);
-	printf("biClrImportant = %d\n",bf->biClrImportant);
 }
 
 
@@ -160,40 +188,44 @@ int main(int argc, char **argv)
 	int fd_bmp;
 	int ret;
 	struct bmpfile cbf;
-
+	//获取BMP文件
 	if(argc <2 ){
 		printf("Wrong use !!!!\n");
 		printf("Usage: %s xxx.bmp\n",argv[0]);
 		goto fail1;
 	}
-
-	//初始化
+	//初始化drm
 	ret = drm_init();
 	if(ret < 0){
 		printf("drm init fail\n");
 		return -1;
 	}
+	//判断是否为bmp文件
 	ret = judge_bmp(argv[1],&cbf);
 	if(ret < 0){
 		printf("something wrong in bmp file %d\n",fd_bmp);
-		goto fail2;
+		goto fail1;
 	}
+	//获取BMP文件的各种信息
 	ret = get_bmp_file(argv[1],&cbf);
 	if(ret < 0){
 		printf("something wrong in bmp file %d\n",fd_bmp);
-		goto fail2;
+		goto fail1;
 	}
-
+	//打印信息
 	show_bmp_info(&cbf);
-	ret = show_bmp(&cbf);
+	//显示画面信息
+	ret = show_bmp(&cbf , buf.width/2 - cbf.biWidth/2, buf.height/2 - cbf.biHeight/2);
 	if(ret < 0){
 		printf("show_bmp wrong!\n");
 		goto fail2;
 	}
-
+	//获取按键，按键后退出程序
 	getchar();
-
-	drm_exit();	
+	//注销drm
+	drm_exit();
+	//释放内存空间
+	free_bmp_file(&cbf);	
 	return 0;
 
 fail2: 
